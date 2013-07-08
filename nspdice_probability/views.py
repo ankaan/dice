7e8409e0a12c08a900c3f0a8fdd083c85451f4c4
Anders Engström <ankan@ankan.eu>
2013-07-08 19:02:45 +0200

    Updated rules, + build your own combination

--- nspdice_probability/views.py
+++ nspdice_probability/views.py
@@ -16,6 +16,42 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.pyplot import Axes
 
+SKILL_DIE = {}
+SKILL_DIE['del'] = Die.const(0)
+
+SKILL_DIE['1'] = Die(6)
+SKILL_DIE['2'] = Die(8)
+SKILL_DIE['3'] = Die(10)
+SKILL_DIE['4'] = Die(12)
+SKILL_DIE['5'] = Die(20)
+SKILL_DIE['6'] = Die(20) + Die.const(6)
+
+SKILL_DIE['seq1'] = Die(4) + Die(6)
+SKILL_DIE['seq2'] = SKILL_DIE['seq1'] + Die(8)
+SKILL_DIE['seq3'] = SKILL_DIE['seq2'] + Die(10)
+SKILL_DIE['seq4'] = SKILL_DIE['seq3'] + Die(12)
+SKILL_DIE['seq5'] = SKILL_DIE['seq4'] + Die(20)
+SKILL_DIE['seq6'] = SKILL_DIE['seq5'] + Die.const(6)
+
+PRO_DIE = {}
+PRO_DIE['del'] = Die.const(0)
+
+PRO_DIE['1'] = Die(4) + Die.const(1)
+PRO_DIE['2'] = Die(6)
+PRO_DIE['3'] = Die(8)
+PRO_DIE['4'] = Die(10)
+PRO_DIE['5'] = Die(12)
+PRO_DIE['6'] = Die(20)
+PRO_DIE['7'] = Die(20) + Die.const(6)
+
+PRO_DIE['seq1'] = Die(4) + Die.const(1)
+PRO_DIE['seq2'] = Die(4) + Die(6)
+PRO_DIE['seq3'] = PRO_DIE['seq2'] + Die(8)
+PRO_DIE['seq4'] = PRO_DIE['seq3'] + Die(10)
+PRO_DIE['seq5'] = PRO_DIE['seq4'] + Die(12)
+PRO_DIE['seq6'] = PRO_DIE['seq5'] + Die(20)
+PRO_DIE['seq7'] = PRO_DIE['seq6'] + Die.const(6)
+
 class ModeForm(forms.Form):
   MODE_CHOICES = (
     ('target','Target'),
@@ -29,27 +65,45 @@ class ModeForm(forms.Form):
 class ColumnForm(forms.Form):
 
   SKILL_CHOICES = (
-    ('del','Delete'),
-    ('pro','Property'),
-    ('Skill', (
+    ('del',''),
+    ('Die Sequence', (
+      ('seq1','LvL 1 (D4-D6)'),
+      ('seq2','LvL 2 (D4-D8)'),
+      ('seq3','LvL 3 (D4-D10)'),
+      ('seq4','LvL 4 (D4-D12)'),
+      ('seq5','LvL 5 (D4-D20)'),
+      ('seq6','LvL 6 (D4-D20+6)'),
+      )),
+    ('Single Die', (
       ('1','LvL 1 (D6)'),
       ('2','LvL 2 (D8)'),
       ('3','LvL 3 (D10)'),
       ('4','LvL 4 (D12)'),
       ('5','LvL 5 (D20)'),
-      ('6','LvL 6 (+6)'),
+      ('6','LvL 6 (D20+6)'),
     )),
   )
-  
+
   PRO_CHOICES = (
-    ('1','1-8   (D6)'),
-    ('2','9-14  (D8)'),
-    ('3','15-17 (D10)'),
-    ('4','18-9  (D12)'),
-    ('5','20    (D20)'),
-    ('6','21-24 (D16)'),
-    ('7','25-29 (D24)'),
-    ('8','30    (D30)'),
+    ('del',''),
+    ('Die Sequence', (
+      ('seq1','1-5 (D4+1)'),
+      ('seq2','6-10 (D4-D6)'),
+      ('seq3','11-15 (D4-D8)'),
+      ('seq4','16-20 (D4-D10)'),
+      ('seq5','21-25 (D4-D12)'),
+      ('seq6','26-29 (D4-D20)'),
+      ('seq7','30 (D4-D20+6)'),
+      )),
+    ('Single Die', (
+      ('1','1-5 (D4+1)'),
+      ('2','6-10 (D6)'),
+      ('3','11-15 (D8)'),
+      ('4','16-20 (D10)'),
+      ('5','21-25 (D12)'),
+      ('6','26-29 (D20)'),
+      ('7','30 (D20+6)'),
+    )),
   )
 
   def _flatten(self,choices):
@@ -73,10 +127,9 @@ class ColumnForm(forms.Form):
   pro =   forms.ChoiceField(choices=PRO_CHOICES)
 
   def keep(self):
-    if not self.is_valid():
-      return True
-    else:
-      return self.cleaned_data['skill'] != 'del'
+    return any([not self.is_valid(),
+                self.cleaned_data['skill'] != 'del',
+                self.cleaned_data['pro'] != 'del'])
 
   def details(self):
     return "%s\n%s"%(self.get_skill_display(),self.get_pro_display())
@@ -167,7 +220,7 @@ def _probability_reference(request, stage):
   if columnmanager.is_valid() and customdiemanager.is_valid():
     # Create the corresponding die for each form.
     dice = [
-      build_die(d['skill'],d['pro'])
+      SKILL_DIE[d['skill']] + PRO_DIE[d['pro']]
       for d in columnmanager.cleaned_data()
     ]
     dice += [ d['die'] for d in customdiemanager.cleaned_data() ]
@@ -225,40 +278,6 @@ def _probability_reference(request, stage):
       'customdiemanager': customdiemanager,
     })
 
-SKILL_DIE = {}
-SKILL_DIE['1'] = Die(4) + Die(6)
-SKILL_DIE['2'] = SKILL_DIE['1'] + Die(8)
-SKILL_DIE['3'] = SKILL_DIE['2'] + Die(10)
-SKILL_DIE['4'] = SKILL_DIE['3'] + Die(12)
-SKILL_DIE['5'] = SKILL_DIE['4'] + Die(20)
-SKILL_DIE['6'] = SKILL_DIE['5'] + Die.const(6)
-
-DB_DIE = {}
-DB_DIE['1'] = Die(6)
-DB_DIE['2'] = Die(8)
-DB_DIE['3'] = Die(10)
-DB_DIE['4'] = Die(12)
-DB_DIE['5'] = Die(20)
-DB_DIE['6'] = Die(16)
-DB_DIE['7'] = Die(24)
-DB_DIE['8'] = Die(30)
-
-PRO_DIE = {}
-PRO_DIE['1'] = Die(4) +       DB_DIE['1']
-PRO_DIE['2'] = PRO_DIE['1'] + DB_DIE['2']
-PRO_DIE['3'] = PRO_DIE['2'] + DB_DIE['3']
-PRO_DIE['4'] = PRO_DIE['3'] + DB_DIE['4']
-PRO_DIE['5'] = PRO_DIE['4'] + DB_DIE['5']
-PRO_DIE['6'] = PRO_DIE['5'] + DB_DIE['6']
-PRO_DIE['7'] = PRO_DIE['5'] + DB_DIE['7']
-PRO_DIE['8'] = PRO_DIE['5'] + DB_DIE['8']
-
-def build_die(skill,pro):
-  if skill == 'pro':
-    return PRO_DIE[pro]
-  else:
-    return SKILL_DIE[skill] + DB_DIE[pro]
-
 def transpose(data):
   if len(data)==0:
     return []
@@ -269,9 +288,8 @@ def transpose(data):
 
 
 def prob_plot(request,dice,columnmanager,customdiemanager,target=False):
-  fig = Figure(figsize=(12, 9),frameon=False)
-  ax = Axes(fig,[0.06, 0.05, 0.77, 0.94])
-  #ax.set_axis_off()
+  fig = Figure(figsize=(12, 6),frameon=False)
+  ax = Axes(fig,[0.07, 0.07, 0.71, 0.91])
   fig.add_axes(ax)
 
   base_forms = columnmanager.base_forms() + customdiemanager.base_forms()
@@ -294,8 +312,6 @@ def prob_plot(request,dice,columnmanager,customdiemanager,target=False):
     ax.set_xlabel('Sum')
   ax.grid(True)
 
-  #box = ax.get_position()
-  #ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
   if dice:
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
