e3df9a9ac6682b09ddab9989254f4567185cee90
Anders Engstrom <ankaan@gmail.com>
2013-07-14 13:42:03 +0200

    Local settings (secret,static files,static root)

--- nspdice/settings.py
+++ nspdice/settings.py
@@ -59,11 +59,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/http/vhost/ankan/html/nspdice/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/nspdice/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -81,7 +81,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
+SECRET_KEY = 'xFmA3BP6ML1O0IctLzsjvbZjhNsKItXk9amTbmwaWHBUOM0Km3/+Lb2AuIiYPn4tAGzNZWag74SR'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
