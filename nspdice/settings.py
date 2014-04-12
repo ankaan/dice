809c14f971b09513bb477ed8ed767b825bf0f76e
Anders Engstrom <ankaan@gmail.com>
2014-04-12 14:23:10 +0200

    Cleaned settings

--- nspdice/settings.py
+++ nspdice/settings.py
@@ -81,7 +81,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'xFmA3BP6ML1O0IctLzsjvbZjhNsKItXk9amTbmwaWHBUOM0Km3/+Lb2AuIiYPn4tAGzNZWag74SR'
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
