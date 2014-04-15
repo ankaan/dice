22a9fe586a7b055513ae5d2f2d6bf05c5ce79cb3
Anders Engström <ankan@ankan.eu>
2014-04-15 16:31:54 +0200

    Cleaned settings from any local data

--- nspdice/settings.py
+++ nspdice/settings.py
@@ -59,11 +59,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/http/vhost/ankan/html/nspdice/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/nspdice/static/'
+STATIC_URL = ''
 
 # Additional locations of static files
 STATICFILES_DIRS = (
